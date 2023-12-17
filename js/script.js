bcb7d5fcc4212491d996484298da50d62e4d9c37
bobcorp <bobcorp99@mail.ru>
2023-12-17 09:12:14 +0500

    rbc-group(my v1.0)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// document.querySelectorAll('.service__main-card').onclick = function () {
+//     window.location.href = '../service.html';
+// }
+// 'use esversion: 6';
+
+// const promise = new Promise((resolve, reject) => {
+//     const randomNumber = Math.random();
+    
+//     if(randomNumber < 0.7) {
+//       resolve('Все прошло отлично!');
+//     } else {
+//       reject(new Error('Что-то пошло не так'));
+//     }
+//   });
+//   promise.then((data) => {
+//     console.log(data);  // вывести 'Все прошло отлично!'
+//     },
+//     (error) => {
+//     console.log(error); // вывести ошибку
+//     }
+//   );
